c46a9470b108e09c813c8babf9fb03149f19f037
Alex Gerus <alexander.gerus@grse.de>
2024-10-12 15:10:26 +0300

    Init

---
+++ src/app/pdf-generator/pdf-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { PdfService } from '../pdf.service';
+
+@Component({
+  selector: 'app-pdf-generator',
+  templateUrl: './pdf-generator.component.html',
+  styleUrls: ['./pdf-generator.component.css']
+})
+export class PdfGeneratorComponent {
+  constructor(private pdfService: PdfService) { }
+
+  generatePdf() {
+    this.pdfService.generatePdf();
+  }
+}
